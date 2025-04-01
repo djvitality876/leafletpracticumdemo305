54cd5e332493f41044f9a7dc66da1f14ef8ede76
Ra Bacchus <vrbacchus@jamcoders.org.jm>
2025-04-01 19:04:49 -0400

    Update google-doc-url.js
    Added random comment

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,6 @@
 // paste in your published Google Sheets URL from the browser address bar
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1Tga_P9qZLh7Nk_dP6-xEwUuhDLFe0JIuWd2cub7xxfo/edit?gid=0#gid=0';
 
+// random comment
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
